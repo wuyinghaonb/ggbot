712ae8ddd6137136f88a3bd02a9023204efca24c
Mac <mac@MacdeMacBook-Air.local>
2024-02-12 19:39:52 +0800

    first commit

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://www.google.com'
-response = requests.get(url)
-
-if response.status_code == 200:
-    print('123')
-else:
-    print('请求失败，状态码：', response.status_code)
