933a72a5db01bdb0e8c4efc3ef14c4a94f315256
wyh <417689923@qq.com>
2024-03-10 00:22:46 +0800

    use os environment

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,7 @@ def main():
     updater = Updater(token=(os.environ['TELEGRAM_ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     global redis1
-    redis1 = redis.Redis(host=(os.environ['REDIS''HOST']), password=(os.environ['REDIS_PASSWORD']),
+    redis1 = redis.Redis(host=(os.environ['REDIS_HOST']), password=(os.environ['REDIS_PASSWORD']),
                          port=(os.environ['REDIS_REDISPORT']))
 
     # You can set this logging module, so you will know when and why things do not work as expected Meanwhile, update your config.ini as:
